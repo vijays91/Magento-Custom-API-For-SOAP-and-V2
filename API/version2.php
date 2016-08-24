67d830a9262629ca7ddbadc6ec62055aab76c5e4
VijayaKumar S <vjai.s91@gmail.com>
2016-08-24 23:22:49 +0530

    Update version2.php

--- API/version2.php
+++ API/version2.php
@@ -1,4 +1,7 @@
 <?php
+
+ini_set("soap.wsdl_cache_enabled", "0"); /* Disable WSDL cache ( cache file in : tmp/ wsdl-***)*/
+
 $api_url_v2 = "http://www.yousite.com/index.php/api/v2_soap/?wsdl=1";
 $username = '*****';
 $password = '*****';
@@ -6,6 +9,9 @@ $password = '*****';
 
 $cli = new SoapClient($api_url_v2);
 $session_id = $cli->login($username, $password);
+
+// $functions = $client->__getFunctions (); /* Used for checking function(new api function) exist or not */
+// print_r($functions);
 
 //** Create 
 // $result = $cli->customapiDemoCreate($session_id, array('name' => 'Test_name', 'pincode' => '123456', 'address' => 'Test_address'));
